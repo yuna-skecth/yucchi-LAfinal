582172ee3ce87d430037d46a7d4e098a16a46815
yuna-skecth <yunat2000.wsg@gmail.com>
2021-03-08 12:40:04 +0900

    0308first-view完成

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+            $('.rightimg-slider').slick({
+                autoplay: true,
+                //                autospeed:3000,
+                dots: false,
+                arrows: false,
+
+            });
+        });
