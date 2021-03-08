1a0d41d2d63d165b7934893a488370b21bd86f1e
yuna-skecth <yunat2000.wsg@gmail.com>
2021-03-08 19:57:57 +0900

    モーダル7割完成

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ $(document).ready(function() {
 
             });
         });
+
+//
+//$(function(){
+//    $('.js-modal-open').on('click',function(){
+//        $('.js-modal').fadeIn();
+//        return false;
+//    });
+//    $('.js-modal-close').on('click',function(){
+//        $('.js-modal').fadeOut();
+//        return false;
+//    });
+//});
+
+
+$(function(){
+    $('.js-modal-open').each(function(){
+        $(this).on('click',function(){
+            var target = $(this).data('target');
+            var modal = document.getElementById(target);
+            $(modal).fadeIn();
+//            $('body').addClass('fixed').css({ top: -scrollPos });//背景固定
+            return false;
+        });
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+//        $('body').removeClass('fixed').css({ top: 0 });//背景固定を解除
+        return false;
+    }); 
+});
